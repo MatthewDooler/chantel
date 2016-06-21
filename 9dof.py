7463e084cac02982bb943f2f92e9dcb99e59ee90
MatthewDooler <MatthewDooler@users.noreply.github.com>
2016-06-21 21:38:34 +0100

    call fusion lib with data

--- 9dof.py
+++ 9dof.py
@@ -5,13 +5,7 @@ sys.path.append(r'/home/pi/git/quick2wire-python-api/')
 from i2clibraries import i2c_hmc5883l
 from i2clibraries import i2c_adxl345
 from i2clibraries import i2c_itg3205
-
-print("magnetometer:")
-magnetometer = i2c_hmc5883l.i2c_hmc5883l(0)
-magnetometer.setContinuousMode()
-magnetometer.setDeclination(1,43) # magnetic declination in degrees west (degrees, minute)
-magnetometer_axes = magnetometer.getAxes()
-print(magnetometer_axes)
+from fusion import fusion
 
 print("accelerometer:")
 accelerometer = i2c_adxl345.i2c_adxl345(0)
@@ -23,3 +17,16 @@ print("gyroscope:")
 gyroscope = i2c_itg3205.i2c_itg3205(0, addr=0x68)
 gyroscope_axes = gyroscope.getAxes()
 print(gyroscope_axes)
+
+print("magnetometer:")
+magnetometer = i2c_hmc5883l.i2c_hmc5883l(0)
+magnetometer.setContinuousMode()
+magnetometer.setDeclination(1,43) # magnetic declination in degrees west (degrees, minute)
+magnetometer_axes = magnetometer.getAxes()
+print(magnetometer_axes)
+
+fusion = fusion.Fusion()
+fusion.update(accelerometer_axes, gyroscope_axes, magnetometer_axes)
+print("heading="+fusion.heading)
+print("pitch="+fusion.pitch)
+print("roll="+fusion.roll)
