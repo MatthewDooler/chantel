c402a76afb929975144164665e8c1d9beb2a3e02
MatthewDooler <MatthewDooler@users.noreply.github.com>
2016-06-21 21:50:51 +0100

    timing

--- 9dof.py
+++ 9dof.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import sys
+import datetime as dt
 import time
-sys.path.append(r'/home/pi/git/quick2wire-python-api/')
 
+sys.path.append(r'/home/pi/git/quick2wire-python-api/')
 from i2clibraries import i2c_hmc5883l
 from i2clibraries import i2c_adxl345
 from i2clibraries import i2c_itg3205
+
 from fusion import fusion
 
 accelerometer = i2c_adxl345.i2c_adxl345(0)
@@ -18,15 +20,20 @@ magnetometer.setContinuousMode()
 magnetometer.setDeclination(1,43) # magnetic declination in degrees west (degrees, minute)
 
 fusion = fusion.Fusion()
+frequency = 50 # Hz
+period = 1.0 / frequency
 
 for x in range(0, 100):
+	start_time = dt.datetime.now()
 	accelerometer_values = accelerometer.getAxes()
 	gyroscope_values = gyroscope.getAxes()
 	magnetometer_values = magnetometer.getAxes()
 	fusion.update(accelerometer_values, gyroscope_values, magnetometer_values)
+	elapsed_seconds = fusion.elapsed_seconds(start_time)
 	if x % 10 == 0:
+		print(elapsed)
 		print("heading="+str(fusion.heading))
 		print("pitch="+str(fusion.pitch))
 		print("roll="+str(fusion.roll))
 		print("")
-	time.sleep(0.1) # TODO: sleep less??
+	time.sleep(0.02) # TODO: sleep less??
