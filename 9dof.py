1fb0b976a7b5859bf7d799cafe3cecc419f8bbba
MatthewDooler <MatthewDooler@users.noreply.github.com>
2016-06-21 19:59:29 +0100

    accelerometer

--- 9dof.py
+++ 9dof.py
@@ -3,7 +3,14 @@ import sys
 sys.path.append(r'/home/pi/git/quick2wire-python-api/')
 
 from i2clibraries import i2c_hmc5883l
-hmc5883l = i2c_hmc5883l.i2c_hmc5883l(0)
-hmc5883l.setContinuousMode()
-hmc5883l.setDeclination(1,43) #  magnetic declination in degrees west (degrees, minute)
-print(hmc5883l)
+from i2clibraries import i2c_adxl345
+
+print("magnetometer:")
+magnetometer = i2c_hmc5883l.i2c_hmc5883l(0)
+magnetometer.setContinuousMode()
+magnetometer.setDeclination(1,43) # magnetic declination in degrees west (degrees, minute)
+print(magnetometer)
+
+print("accelerometer:")
+accelerometer = i2c_adxl345.i2c_adxl345(0)
+print(accelerometer)
