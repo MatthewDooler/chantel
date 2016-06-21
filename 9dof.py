b3f4914b7dc88039766ce8e0ae84bfa0d2ccc83c
MatthewDooler <MatthewDooler@users.noreply.github.com>
2016-06-21 23:03:05 +0100

    convert heading to degrees

--- 9dof.py
+++ 9dof.py
@@ -63,7 +63,7 @@ for x in range(0, frequency*duration):
 		#print("pitch="+str(fusion.pitch))
 		#print("roll="+str(fusion.roll))
 		#print("")
-		print("heading = %.0f°, pitch = %.0f°, roll = %.0f°" % round(fusion.heading*degree, 0), round(fusion.pitch*degree, 0), round(fusion.roll*degree, 0))
+		print("heading = %.0f°, pitch = %.0f°, roll = %.0f°" % (round(fusion.heading*degree, 0), round(fusion.pitch*degree, 0), round(fusion.roll*degree, 0)))
 	if elapsed > period:
 		print("running slow (period="+str(period)+", elapsed="+str(elapsed)+")")
 	else:
