8963ca3e0dd1f341eaf432d35744de8ab92ed2a8
MatthewDooler <MatthewDooler@users.noreply.github.com>
2016-06-25 20:33:52 +0100

    reduce prop speed

--- 9dof.py
+++ 9dof.py
@@ -96,7 +96,7 @@ for i in range(0, frequency*duration):
 	#fusion.update_nomag(accelerometer_values, gyroscope_values)
 	#fusion.update(accelerometer_values, (0,0,0), magnetometer_values)
 
-	throttle = 95
+	throttle = 10
 	yaw_offset = 0 # TODO: make sure positive goes CW for sanity purposes
 	
 	if pitch <= -1:
