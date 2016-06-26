4a3b9cc56e6b843047447c27631401717acee648
MatthewDooler <MatthewDooler@users.noreply.github.com>
2016-06-26 11:33:37 +0100

    ui reconnects on failure

--- www/js/sandbox.js
+++ www/js/sandbox.js
@@ -1,52 +1,56 @@
 $(function() {
+    
+    var connecting = false
+    var connected = false
     var attitude = $.flightIndicator('#attitude', 'attitude', {roll:0, pitch:0, size:800, showBox:false});
     var heading = $.flightIndicator('#heading', 'heading', {heading:0, size:200, showBox:true});
     var variometer = $.flightIndicator('#variometer', 'variometer', {vario:-5, showBox:true});
     var airspeed = $.flightIndicator('#airspeed', 'airspeed', {showBox:true});
     var altimeter = $.flightIndicator('#altimeter', 'altimeter');
 
-    // Update at 20Hz
-    // var increment = 0;
-    // setInterval(function() {
-        
-    //     heading.setHeading(0);
-    //     attitude.setRoll(0);
-    //     attitude.setPitch(0); // nose up/down
-        
-    //     // vertical speed
-    //     variometer.setVario(0);
-        
-    //     airspeed.setAirSpeed(0);
-        
-    //     altimeter.setAltitude(223);
-    //     altimeter.setPressure(1000);
-    //     increment++;
-    // }, 50);
+    // Check connection every second (to reconnect if it's closed)
+    setInterval(function() {
+        if(!connected && !connecting) {
+            connecting = true
+            console.log("connecting")
+            sock = new WebSocket("ws://fraybentos.heart:8080/");
+            // sock.send(message);
+            // sock.close();
+            sock.onopen = function(evt) {
+                console.log('open');
+                $("#connection-state").toggleClass("connected", true).toggleClass("disconnected", false).html("Connected")
+                connected = true
+                connecting = false
+            };
+            sock.onclose = function(evt) {
+                console.log("close\n");
+                $("#connection-state").toggleClass("connected", false).toggleClass("disconnected", true).html("Disconnected")
+                connected = false
+                connecting = false
+            };
+            sock.onmessage = function(evt) {
+                message = JSON.parse(evt.data);
 
-    sock = new WebSocket("ws://fraybentos.heart:8080/");
-    // sock.send(message);
-    // sock.close();
-    sock.onopen = function(evt) {
-        console.log('open');
-    };
-    sock.onclose = function(evt) {
-        console.log("close\n");
-    };
-    sock.onmessage = function(evt) {
-        message = JSON.parse(evt.data)
-        heading.setHeading(message.heading);
-        attitude.setRoll(message.pitch);
-        attitude.setPitch(-message.roll);
+                headingValue = message.heading;
+                pitchValue = message.pitch;
+                rollValue = -message.roll;
 
-        variometer.setVario(0); // vertical speed
-        airspeed.setAirSpeed(0);
-        altimeter.setAltitude(223);
-        altimeter.setPressure(1000);
-    };
-    sock.onerror = function(evt) {
-        console.log('error: ' + evt.data + '\n');
-        sock.close();
-    };
+                heading.setHeading(headingValue);
+                attitude.setRoll(pitchValue);
+                attitude.setPitch(rollValue);
+
+                variometer.setVario(0); // vertical speed
+                airspeed.setAirSpeed(0);
+                altimeter.setAltitude(223);
+                altimeter.setPressure(1000);
+            };
+            sock.onerror = function(evt) {
+                console.log('error: ' + evt.data + '\n');
+                sock.close();
+            };
+        }
+    }, 1000);
+
 });
 
 
