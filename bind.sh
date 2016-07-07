47741b4e7096b6b4cd896af90470377ff437a762
MatthewDooler <MatthewDooler@users.noreply.github.com>
2016-07-07 21:19:46 +0100

    startup script

--- bind.sh
+++ bind.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
-lt --port 8080 --subdomain chantel | at now + 1 minutes
-lt --port 8081 --subdomain chantelws | at now + 1 minutes
+#!/usr/bin/env sh
+lt --port 8080 --subdomain chantel &
+lt --port 8081 --subdomain chantelws &
---
+++ startup.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+at -f ./bind.sh now + 1 minutes
