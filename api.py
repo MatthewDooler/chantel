dcf9860864f8bd4f085dfeec46265707464168f0
Matthew Dooler <dooler.matthew@gmail.com>
2020-01-03 17:55:18 +0000

    Fix shutdown

--- api.py
+++ api.py
@@ -48,4 +48,4 @@ def restart():
 	os.system('kill %d' % os.getpid())
 
 def shutdown():
-	os.system("shutdown now -h") # TODO: doesn't work on rpi, `shutdown` not available in sh shell
+	os.system("shutdown -h NOW") # TODO: doesn't work on rpi, `shutdown` not available in sh shell
